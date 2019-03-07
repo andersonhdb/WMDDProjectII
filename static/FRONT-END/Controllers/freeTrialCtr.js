d1ad3f4145af154b8a4bb897506f388041cb4efa
Anderson Borba <andersonhborba@hotmail.com>
2019-03-06 20:42:01 -0800

    modified th freetrial.html and freetrialCrtl.js to implement signup errors functionality

--- static/FRONT-END/Controllers/freeTrialCtr.js
+++ static/FRONT-END/Controllers/freeTrialCtr.js
@@ -5,5 +5,55 @@ angular.module('freeTrialController', []).controller('freeTrialCtr', ['$scope', 
   $scope.signIn = function signIn(){
     window.location = "/#!/requests";
   };
-
+  angular.module('freeTrialController', []).controller('freeTrialCtr', ['$scope', '$rootScope', '$http', '$route', '$window', function($scope, $rootScope, $http, $route, $window){
+    $rootScope.css = $route.current.$$route.css;
+    $rootScope.logged = true;
+    $scope.account = {
+      username: undefined,
+      email: undefined,
+      password: undefined
+    };
+  
+    $scope.signIn = function signIn(){
+      console.log($scope.account)
+      const user = $scope.account;
+      for(var element in user){
+        if(user[element]== undefined){
+          $scope.signup_alert = {
+            title: "Form field not filled properly",
+            info: `please fill up the ${element} field of the form`
+          };
+          jQuery('#signup_alert').modal('toggle');
+          return;
+        }
+      };
+      if($scope.confirmPassword == undefined){
+        $scope.signup_alert = {
+          title: "Form field not filled properly",
+          info: `please fill up the confirm password field of the form`
+        };
+        jQuery('#signup_alert').modal('toggle');
+        return;
+      }
+      else if($scope.confirmPassword != user.password){
+        $scope.signup_alert = {
+          title: "Passwords Do not match",
+          info: `the confirmation of your password`
+        };
+        jQuery('#signup_alert').modal('toggle');
+        return;
+      }
+      else{
+        //$http.post('/signupManager',user)
+      }
+      // if(user.password == $scope.confirmPassword){
+      //    window.location = "/#!/requests";
+      //  }
+      // else{
+  
+      // }
+    };
+  
+  }]);
+  
 }]);
